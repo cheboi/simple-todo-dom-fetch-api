a425be820c43f2906680a2116062fa35f49c2c6b
cheboi <moscheb96@gmail.com>
2022-10-24 03:14:49 +0300

    implemented fetch api

--- src/app.js
+++ src/app.js
@@ -4,23 +4,46 @@ document.addEventListener('DOMContentLoaded', function(){
     const form =document.querySelector('form.todo');
     const list = document.getElementById("tasks");
     let complete = document.getElementById("tasks2");
-    let Check = document.querySelectorAll(".checkBox");
-
+    
+    let todoData = []; 
     function getAllTodos(){
         return new Promise((resolve => {
             fetch(new Request(todoUrl)).
             then( res => {
                 return res.json();
             })
-            .then(data => {resolve(data);});
+            .then(data => {resolve(data);
+                localStorage.setItem('todoData', JSON.stringify(data))
+            });
         }))
     }
+
+    getAllTodos();
+  
+    todoData = JSON.parse(localStorage.getItem("todoData"));
+    console.log(todoData)
+
+    const addData = () => {
+
+        todoData.unshift({
+          text: textInput.value,
+          completed:false,
+        });
+      
+        localStorage.setItem("todoData", JSON.stringify(todoData));
+        saveTodo();
+      };
+
+    (() => {
+        todoData = JSON.parse(localStorage.getItem("todoData"));
+        saveTodo();
+    })();
 
     function saveTodo(title){
         let req = new  Request(todoUrl, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({title: title})
+            body: JSON.stringify({title: title })
         });
 
         return new Promise(resolve => {
@@ -29,21 +52,6 @@ document.addEventListener('DOMContentLoaded', function(){
                 .then(data => { resolve(data); });
         })
     }
-
-    // function completedTodo(){
-    //     let req = new  Request(todoUrl, {
-    //         method: 'PATCH',
-    //         headers: {'Content-Type': 'application/json'},
-    //         body: JSON.stringify({completed: true})
-    //     });
-
-    //     return new Promise(resolve => {
-    //         fetch(req)
-    //             .then(res => { return res.json(); })
-    //             .then(data => { resolve(data); });
-    //     })
-    // }
-
 
     function appendTitleToList(todo){
         if(todo.completed === false){
@@ -54,17 +62,28 @@ document.addEventListener('DOMContentLoaded', function(){
                 </div>
                 <div class="list-elem-body">
                     <span class="text">${todo.title}</span>
-                    <i class="fa fa-edit"></i>
                     <span class="check">Completed</span>
                     <span class="delete">x</span>
                 </div>
             </li>`
-        }        
+        }      
     }
 
     getAllTodos().then(todos => {
         todos.forEach(todo => { appendTitleToList(todo);});
     })
+
+{/* <i class="fa fa-edit"></i> */}
+    
+    // const saveTodo=()=>{
+    //     list.innerHTML=""
+    //     complete.innerHTML=""
+    //     todoData.forEach((a, b)=>{
+    //             appendTitleToList(a, b)
+        
+    //         }
+    //     )
+    // }
 
     function completedTodoList(todo){
         if(todo.completed === true){
@@ -75,8 +94,6 @@ document.addEventListener('DOMContentLoaded', function(){
                 </div>
                 <div class="list-elem-body">
                     <span class="text">${todo.title}</span>
-                    <i class="fa fa-edit"></i>
-                    <span class="delete">x</span>
                 </div>
             </li>`
         }
@@ -93,15 +110,6 @@ document.addEventListener('DOMContentLoaded', function(){
         });
         form.reset();
     })
-    
-//     var stringifyTodos = JSON.stringify(todoUrl);
-// sessionStorage.setItem("todosUrl", stringifyTodos);
-// document.getElementById("userArray").innerHTML = 
-//     sessionStorage.getItem("todosUrl");
-// var retrieveTodos = 
-//     JSON.parse(sessionStorage["todosUrl"])
-// document.getElementById("retrieveIndex").innerHTML 
-//     = retrieveTodos[0];
 
     list.addEventListener('click', function(e){
         if(e.target.classList.contains('delete')){
@@ -115,8 +123,11 @@ document.addEventListener('DOMContentLoaded', function(){
             fetch( new Request(`$todoUrl/${id}`, {
                 method: 'PATCH', 
                 headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({completed: true})}));
+                body: JSON.stringify({completed: true})}))
+                .then((response) => response.json())
+                .then((json) => console.log(json));
             document.querySelector(`li[data-id="${id}"]`).outerHTML = "";
+
         }
     })
     
